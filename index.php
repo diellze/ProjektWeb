2dd6077443e99c59aa2238b27c8e2d2d2a357b0f
Djellzahel <djellza.helshani.9@gmail.com>
2022-04-16 15:56:47 +0200

    init

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+  <!-- Glidejs -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/css/glide.core.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/css/glide.theme.css">
+
+  <!-- Custom StyleSheet -->
+  <link rel="stylesheet" href="./styles.css" />
+  <title>Ecommerce Website</title>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <?php include 'header.php'?>
+
+  <div class="hero">
+    <div class="left">
+      <span>Oferta Ekskluzive</span>
+      <h1>DERI NË 50% ZBRITJE</h1>
+      <small>Përfitoni nga të gjitha ofertat eksluzive sezonale</small>
+      <a href="">Shiko Koleksionet </a>
+    </div>
+    <div class="right">
+      <img src="img/icon1.png" alt="" />
+    </div>
+  </div>
+
+  <section class="section promotion">
+    <div class="title">
+      <h2>Koleksionet</h2>
+    </div>
+
+    <div class="promotion-layout container">
+      <div class="promotion-item">
+        <img src="img/promo1.png" alt="" />
+        <div class="promotion-content">
+          <h3>KËPUCË ME TAKA</h3>
+          <a href="">BLEJ TANI</a>
+        </div>
+      </div>
+
+      <div class="promotion-item">
+        <img src="img/promo2.png" alt="" />
+        <div class="promotion-content">
+          <h3>ATLETE</h3>
+          <a href="">BLEJ TANI</a>
+        </div>
+      </div>
+
+      <div class="promotion-item">
+        <img src="img/promo3.png" alt="" />
+        <div class="promotion-content">
+          <h3>ÇIZME</h3>
+          <a href="">BLEJ TANI</a>
+        </div>
+      </div>
+
+      <div class="promotion-item">
+        <img src="img/promo4.png" alt="" />
+        <div class="promotion-content">
+          <h3>SANDALE</h3>
+          <a href="">BLEJ TANI</a>
+        </div>
+      </div>
+  </section>
+
+  <!-- Products -->
+  <section class="section products">
+    <div class="title">
+      <h2>Produktet e Reja</h2>
+    </div>
+
+    <div class="product-layout">
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product1.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product2.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product3.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product4.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product5.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product6.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product7.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="img-container">
+          <img src="img/product8.png" alt="" />
+          <div class="addCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+        <div class="bottom">
+          <a href="">Emri i Produktit</a>
+          <div class="price">
+            <span>100 €</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="section advert">
+    <div class="advert-layout container">
+      <div class="item ">
+        <img src="img/promo7.png" alt="">
+        <div class="content left">
+          <span>Çmime eksluzive</span>
+          <h3>Koleksioni Dimëror</h3>
+          <span>Shiko Koleksionin</span>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/promo8.png" alt="">
+        <div class="content  right">
+          <span>Trendet e Reja</span>
+          <h3>Brendet e Atleteve</h3>
+          <span>Blej tani </span>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <?php include 'footer.php'?>
+  <script src="./index.js"></script>
+  <!-- djell-->
+</body>
+</html>
---
+++ produktet.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+  
+  <!-- Custom StyleSheet -->
+  <link rel="stylesheet" href="./styles.css" />
+  <title>Ecommerce Website</title>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  
+    <?php include 'header.php'?>
+
+  <!-- PRODUCTS -->
+
+  <section class="section products">
+    <div class="products-layout container">
+      <div class="col-1-of-4">
+        <div>
+          <div class="block-title">
+            <h2>Kategoritë</h2>
+          </div>
+          <ul class="block-content">
+            <li>
+              <input type="checkbox" name="" id="">
+              <label for="">
+                <span>Atlete</span>
+              </label>
+            </li>
+
+            <li>
+              <input type="checkbox" name="" id="">
+              <label for="">
+                <span>Çizme</span>
+              </label>
+            </li>
+
+            <li>
+              <input type="checkbox" name="" id="">
+              <label for="">
+                <span>Këpucë</span>
+              </label>
+            </li>
+
+            <li>
+              <input type="checkbox" name="" id="">
+              <label for="">
+                <span>Sandale</span>
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-3-of-4">
+        <form action="">
+          <div class="item">
+          </div>
+          <a href="">Filtro produktet</a>
+        </form>
+
+        <div class="product-layout">
+
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product1.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product2.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product3.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product4.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product5.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product6.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product7.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product8.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product9.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product10.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product11.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product12.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product13.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product14.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+          <div class="product">
+            <div class="img-container">
+              <img src="img/product15.png" alt="" />
+              <div class="addCart">
+                <i class="fas fa-shopping-cart"></i>
+              </div>
+            </div>
+            <div class="bottom">
+              <a href="productDetails.html">Emri i Produktit</a>
+              <div class="price">
+                <span>100 €</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <ul class="pagination">
+          <span>1</span>
+          <span>2</span>
+          <span class="icon">></span>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+    <?php include 'footer.php'?>
+  <script src="./index.js"></script>
+</body>
+</html>
